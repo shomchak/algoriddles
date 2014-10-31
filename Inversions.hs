b08e9d84c8bad9ad6706fa3c541d552529016de9
shomchak <shomik.chakravarty@gmail.com>
2014-10-31 16:39:08 -0700

    WIP for faster inversions with tail calls

--- Inversions.hs
+++ Inversions.hs
@@ -1,5 +1,7 @@
-module Inversions (inversions) where
+module Inversions where
 import Data.List (splitAt)
+import Data.Sequence ( (|>), (<|), (><) )
+import qualified Data.Sequence as DS
 
 -- | Return the number of inversions in a list.
 inversions :: Ord a => [a] -> Int
@@ -26,3 +28,39 @@ mergeCount left right
           r                         = head right
           (left_rest,  left_count)  = mergeCount (tail left) right
           (right_rest, right_count) = mergeCount left $ tail right
+
+
+
+
+
+-- sortCount' :: O rd a => [a] -> Int -> ([a], Int)
+-- sortCount' [] n = ([], n)
+-- sortCount' (x:[]) n = ([x], n)
+-- sortCount' xs =
+
+-- | Return the number of inversions in a list.
+inversions' :: Ord a => [a] -> Int
+inversions' = snd . sortCount' . DS.fromList
+
+-- | Sort a list using merge sort, and count the inversions.
+sortCount' :: Ord a => DS.Seq a -> (DS.Seq a, Int)
+sortCount' DS.empty     = (DS.empty,  0)
+sortCount' (x <| DS.empty) = (DS.singleton x, 0)
+sortCount' xs     = (l,   split_count + left_count + right_count)
+  where (left,    right)       = DS.splitAt (DS.length xs `quot` 2) xs
+        (left_l,  left_count)  = sortCount' left
+        (right_l, right_count) = sortCount' right
+        (l,       split_count) = mergeCount' left_l right_l
+
+-- | Merge two sorted lists, count the split inversions.
+mergeCount' :: Ord a => DS.Seq a -> DS.Seq a -> (DS.Seq a, Int)
+mergeCount' = mergeCountIter DS.empty 0
+
+mergeCountIter :: Ord a => DS.Seq a -> Int -> DS.Seq a -> DS.Seq a -> (DS.Seq a, Int)
+mergeCountIter result n xs DS.empty = (result >< xs, n)
+mergeCountIter result n DS.empty ys = (result >< ys, n)
+mergeCountIter result n left right
+  | l <= r    = mergeCountIter (result |> l) n (DS.tail left) right
+  | otherwise = mergeCountIter (result |> r) (n + length left) left (DS.tail right)
+    where l = DS.head left
+          r = DS.head right
