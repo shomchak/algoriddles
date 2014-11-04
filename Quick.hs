8aa0ef86873ddabb29cf0cc4a650ba9f0afdba23
Shomik Chakravarty <shomik.chakravarty@planet-labs.com>
2014-11-03 18:34:12 -0800

    first quicksort implementation

---
+++ Quick.hs
@@ -0,0 +1,14 @@
+module Quick (sort) where
+import Data.List (splitAt)
+import Control.Monad (when)
+
+-- | Quick sort.
+sort :: Ord a => [a] -> [a]
+sort [] = []
+sort [x] = [x]
+sort xs = sort lessers ++ [pivot] ++ sort greaters
+  where p_index = (length xs `quot` 2)
+        (lefts, (pivot:rights)) = splitAt p_index xs
+        rest = lefts ++ rights
+        lessers = filter (<=pivot) rest
+        greaters = filter (>pivot) rest
