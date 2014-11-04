18b0cf41d5a634434c98a61fe5021960473d1694
Shomik Chakravarty <shomik.chakravarty@planet-labs.com>
2014-11-03 18:42:05 -0800

    use first element as pivot

--- Quick.hs
+++ Quick.hs
@@ -3,10 +3,7 @@ import Data.List (splitAt)
 
 -- | Quick sort.
 sort :: Ord a => [a] -> [a]
-sort []  = []
-sort xs  = sort lessers ++ [pivot] ++ sort greaters
-  where p_index                 = (length xs `quot` 2)
-        (lefts, (pivot:rights)) = splitAt p_index xs
-        rest                    = lefts ++ rights
-        lessers                 = filter (<=pivot) rest
-        greaters                = filter (>pivot) rest
+sort []            = []
+sort (pivot:rest)  = sort lessers ++ [pivot] ++ sort greaters
+  where lessers  = filter (<=pivot) rest
+        greaters = filter (>pivot)  rest
