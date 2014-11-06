35dc240422ac3bcd337c26a239ece4a7330ede25
Shomik Chakravarty <shomik.chakravarty@planet-labs.com>
2014-11-05 19:25:51 -0800

    style

--- QuickSeq.hs
+++ QuickSeq.hs
@@ -1,6 +1,11 @@
 -- Implementations of quick sort using sequences under the hood.
 
-module QuickSeq (sort, ) where
+module QuickSeq ( sort
+                , sortSeq
+                , sort'
+                , sortSeq'
+                ) where
+
 import qualified Data.Sequence as S
 import Data.Sequence (ViewL((:<)), (><))
 import qualified Data.Foldable as F
@@ -30,37 +35,35 @@ sortSeq' xs = case S.viewl xs of
 
 
 -- | A type representing the state of partitioning of a specific partition
--- scheme where the pivot is the first element. For a PartitionState i j S.Seq a,
+-- scheme where the pivot is the first element. For a PS i j S.Seq a,
 -- i is the index of the last value <= the pivot (starts at 0, the pivot);
 -- j is the index of the last value processed (starts at 0).
 -- The hiding of the value constructor in combination with provided methods
--- initPartition and stepPartition guarantee that i and j have valid values.
-data PartitionState a = PartitionState Int Int (S.Seq a)
+-- initP and stepP guarantee that i and j have valid values.
+data PartitionState a = PS Int Int (S.Seq a)
                         deriving (Show, Read, Eq)
 
 -- | Produce a the initial state of a partition computation from a list.
-initPartition :: Ord a => S.Seq a -> PartitionState a
-initPartition = PartitionState 0 0
+initP :: Ord a => S.Seq a -> PartitionState a
+initP = PS 0 0
 
 -- | Perform one step of the partition computation.
-stepPartition :: Ord a => PartitionState a -> PartitionState a
-stepPartition (PartitionState i j xs) =
-  case S.viewl xs of
-    S.EmptyL -> PartitionState i j xs
-    pivot:<_ -> case S.length xs == (j+1) of
-      True  -> PartitionState i j xs
-      False -> case S.index xs (j+1) > pivot of
-        True  -> PartitionState i (j+1) xs
-        False -> PartitionState (i+1) (j+1) $
-          S.update j' (S.index xs i') (S.update i' (S.index xs j') xs)
-            where i' = i+1
-                  j' = j+1
+stepP :: Ord a => PartitionState a -> PartitionState a
+stepP (PS i j xs) = case S.viewl xs of
+  S.EmptyL -> PS i j xs
+  pivot:<_ -> case S.length xs == (j+1) of
+    True  -> PS i j xs
+    False -> case S.index xs (j+1) > pivot of
+      True  -> PS i (j+1) xs
+      False -> PS (i+1) (j+1) $
+        S.update j' (S.index xs i') (S.update i' (S.index xs j') xs)
+          where i' = i+1
+                j' = j+1
 
--- | Partition a list with the pivot as the first element.
+-- | Partition a Sequence with the pivot as the first element.
 partition :: Ord a => S.Seq a -> (S.Seq a, S.Seq a)
 partition xs = case S.viewl xs of
   S.EmptyL -> (S.empty, S.empty)
   _        -> S.splitAt i rest
-    where PartitionState i _ sorted =
-            iterate stepPartition (initPartition xs) !! S.length xs
-          (_:<rest) = S.viewl sorted
+    where PS i _ sorted = iterate stepP (initP xs) !! S.length xs
+          (_:<rest)     = S.viewl sorted
