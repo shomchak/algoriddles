67755fad1bf50dd296f9e45b89b4baaa78ce6e8b
shomchak <shomik.chakravarty@gmail.com>
2014-11-06 21:18:10 -0800

    modified partition to return count

--- Quick.hs
+++ Quick.hs
@@ -11,7 +11,13 @@ sort (pivot:rest) = sort lessers ++ [pivot] ++ sort greaters
 sort' :: Ord a => [a] -> [a]
 sort' []           = []
 sort' xs@(pivot:_) = sort' lessers ++ [pivot] ++ sort greaters
-  where (lessers, greaters) = partition xs
+  where (lessers, greaters, _) = partition xs
+
+-- sortCount :: Ord a => [a] -> Int
+-- sortCount [] = 0
+-- sortCount xs@(pivot:_) = g
+--   where (lessers, greaters) = partition xs
+
 
 -- | A type representing the state of partitioning of a specific partition
 -- scheme where the pivot is the first element. For a PartitionState i j [a],
@@ -37,11 +43,16 @@ stepPartition (PartitionState i j xs@(pivot:_)) =
       False -> PartitionState (i+1) (j+1) $ swap (i+1) (j+1) xs
 
 -- | Partition a list with the pivot as the first element.
-partition :: Ord a => [a] -> ([a], [a])
-partition [] = ([], [])
-partition xs = splitAt i rest
+partition :: Ord a => [a] -> ([a], [a], Int)
+partition [] = ([], [], 0)
+partition xs = (left, right, count)
   where PartitionState i _ (_:rest) =
-          iterate stepPartition (initPartition xs) !! length xs
+          iterate stepPartition (initPartition xs) !! (count + 1)
+        (left, right)               = splitAt i rest
+        count                       = length xs - 1
+
+-- partitionCount :: Ord a => [a] -> ([a], [a], Int)
+-- partitionCount xs = (left, right, count)
 
 -- | Swap two elements of a list. This function is partial as it is undefined
 -- for list indices outside the range [0, length-1], but it only called from
@@ -51,3 +62,4 @@ swap i j xs = zipWith new_el [0..length xs - 1] xs
   where new_el k x | k == i    = xs !! j
                    | k == j    = xs !! i
                    | otherwise = x
+
