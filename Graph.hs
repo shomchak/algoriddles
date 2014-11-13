6808407f72e3c4072c1ebdc8884b228bb1925efa
Shomik Chakravarty <shomik.chakravarty@planet-labs.com>
2014-11-12 19:08:42 -0800

    added fromList

--- Graph.hs
+++ Graph.hs
@@ -3,3 +3,6 @@ module Graph where
 -- | Unweighted, undirected, no parallel edges Graph. Represented as an
 -- adjacency list.
 data UnweightedGraph a = UGraph [[a]]
+
+fromList :: Eq a => [[a]] -> UnweightedGraph a
+fromList = UGraph
